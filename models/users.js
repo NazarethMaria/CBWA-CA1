030afee9294fa20264e9f7e16e0319ef688501de
NazarethMaria <62656847+NazarethMaria@users.noreply.github.com>
2020-10-31 20:41:39 +0000

    my modifications again1

--- models/users.js
+++ models/users.js
@@ -2,6 +2,20 @@ const db = require('../db')();
 const COLLECTION = "users";
 
 module.exports = () => {
+
+    const getByKey = async (key) => {
+        if (!key) {
+            console.log(" 01: Missing key");
+            return null;
+        }
+        const users = await db.get(COLLECTION, {key});
+        if (users.length !== 1) {
+            console.log(" 02: Bad key");
+            return null;
+        }
+        return users[0];
+    };
+
     const get = async (email = null) => {
         console.log('    inside users model');
         if (!email) {
@@ -26,7 +40,8 @@ module.exports = () => {
     
     return {
      get,
-     add   
+     add,
+     getByKey 
     }
 };
 
